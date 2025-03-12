091ab25e1a158e8f27792627ad8527e1b6909366
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2025-03-12 11:09:40 +0100

    Home screen UI implementation

---
+++ components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { View, Text, Image, TextInput } from "react-native";
+import React, { FC } from "react";
+import { icons } from "@/constants/icons";
+
+interface SearchBarProps { 
+    onPress?: () => void;
+    placeholder: string;
+}
+
+const SearchBar: FC<SearchBarProps> = ({onPress, placeholder}) => {
+
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor="#ab8bff"
+        className="flex-1 text-white ml-2"
+
+        onPress={onPress}
+        value=""
+        onChangeText={() => {}}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
